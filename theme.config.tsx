d5fbb32933cae793fe9a6727f6b8e9b774a727e3
Fear <ryan.m.lippman@gmail.com>
2024-12-10 16:46:49 -0500

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Snow Docs</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/Fear-Itself/snowmc',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.gg/PgnxEAcTTW',
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Snow MC Docs',
   },
 }
 
